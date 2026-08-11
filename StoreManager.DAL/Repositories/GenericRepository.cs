c5f22e28968c3e88b1583a5ed061c854c2a6e68b
Francesco De Santis <francescods.246@gmail.com>
2026-08-11 17:25:19 +0200

    feat: add GenericRepository

---
+++ StoreManager.DAL/Repositories/GenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoreManager.DAL.Data;
+using StoreManager.DAL.Repositories.Interfaces;
+
+namespace StoreManager.DAL.Repositories
+{
+    internal class GenericRepository<T>(StoreDbContext context) : IGenericRepository<T>
+        where T : class 
+    {
+        private readonly DbSet<T> _dbSet = context.Set<T>();
+
+        public async Task<T?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            return await _dbSet.FindAsync([id], cancellationToken);
+        }
+
+        public async Task<IReadOnlyList<T?>> GetAllAsync(CancellationToken cancellationToken = default)
+        {
+            return await _dbSet.ToListAsync(cancellationToken);
+        }
+
+        public async Task AddAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            await _dbSet.AddAsync(entity, cancellationToken);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+    }
+}
