59d993531f3a8f0bf5e32bd0ef973f2d80eded54
Francesco De Santis <francescods.246@gmail.com>
2026-08-11 14:43:26 +0200

    feat: add packages and references

--- StoreManager.PL.MVC/obj/Debug/net10.0/StoreManager.PL.MVC.AssemblyInfo.cs
+++ StoreManager.PL.MVC/obj/Debug/net10.0/StoreManager.PL.MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreManager.PL.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3637775280ba38055798337a7b9c49b8c847c09f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c816d31cdc3f3e289511d07bd881304f9683a1b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreManager.PL.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreManager.PL.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
