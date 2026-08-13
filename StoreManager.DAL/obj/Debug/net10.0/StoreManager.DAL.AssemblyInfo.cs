22f5277c7d0ccbb0f9571d86d6d46fdc61a1d597
Francesco De Santis <francescods.246@gmail.com>
2026-08-13 16:31:52 +0200

    feat: add 'MappingProfile'

--- StoreManager.DAL/obj/Debug/net10.0/StoreManager.DAL.AssemblyInfo.cs
+++ StoreManager.DAL/obj/Debug/net10.0/StoreManager.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreManager.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92e7709e78a25d09bc5c433cd1608a34baf674d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9554f879d97ca3b400416a7bd8253a6be2b3dbc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreManager.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreManager.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ StoreManager.PL.API/Configurations/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using StoreManager.BLL.Models;
+using StoreManager.DAL.Entities;
+
+namespace StoreManager.PL.API.Configurations
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Address, AddressModel>().ReverseMap();
+            CreateMap<Category, CategoryModel>().ReverseMap();
+            CreateMap<Customer, CustomerModel>().ReverseMap();
+            CreateMap<Order, OrderModel>().ReverseMap();
+            CreateMap<OrderItem, OrderItemModel>().ReverseMap();
+            CreateMap<Payment, PaymentModel>().ReverseMap();
+            CreateMap<Product, ProductModel>().ReverseMap();
+            CreateMap<Review, ReviewModel>().ReverseMap();
+        }
+    }
+}
