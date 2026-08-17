79b69d7e21fa29db8f3f9bde2f028fb8c32787b0
Francesco De Santis <francescods.246@gmail.com>
2026-08-17 14:53:56 +0200

    feat: initial 'Program.cs'

--- StoreManager.PL.API/Program.cs
+++ StoreManager.PL.API/Program.cs
@@ -1,6 +1,37 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+using StoreManager.BLL.Models;
+using StoreManager.BLL.Services;
+using StoreManager.BLL.Services.Interfaces;
+using StoreManager.DAL.Data;
+using StoreManager.DAL.Entities;
+using StoreManager.DAL.Repositories;
+using StoreManager.DAL.Repositories.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<StoreDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<IOrderItemRepository, OrderItemRepository>();
+
+builder.Services.AddScoped<IGenericService<CustomerModel>, GenericService<Customer, CustomerModel>>();
+builder.Services.AddScoped<IGenericService<AddressModel>, GenericService<Address, AddressModel>>();
+builder.Services.AddScoped<IGenericService<CategoryModel>, GenericService<Category, CategoryModel>>();
+builder.Services.AddScoped<IGenericService<ProductModel>, GenericService<Product, ProductModel>>();
+builder.Services.AddScoped<IGenericService<OrderModel>, GenericService<Order, OrderModel>>();
+builder.Services.AddScoped<IGenericService<PaymentModel>, GenericService<Payment, PaymentModel>>();
+builder.Services.AddScoped<IGenericService<ReviewModel>, GenericService<Review, ReviewModel>>();
+
+builder.Services.AddScoped<IOrderItemService, OrderItemService>();
+
+builder.Services.AddAutoMapper(cfg => { }, typeof(Program).Assembly);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -12,6 +43,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
